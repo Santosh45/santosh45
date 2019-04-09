4fb7ccd716c95b18230793f756acf31edc15fde5
Santosh45 <49426768+Santosh45@users.noreply.github.com>
2019-04-09 13:48:09 +0500

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,13 @@
+class Task1{
+public static void main(String args[]){
+String s="Santosh Kumar";
+int rollno=45;
+int marks=770;
+float percent=70;
+
+System.out.println(s);
+System.out.println(rollno);
+System.out.println(marks);
+System.out.println(percent);
+}
+}
---
+++ Task2.java
@@ -0,0 +1,10 @@
+class Task2{
+public static void main(String args[]){
+	double d=75.25;
+	int i=(int)d;
+	float f=(float)d-i;
+	System.out.println(d);
+	System.out.println(i);
+	System.out.println(f);
+}
+}
